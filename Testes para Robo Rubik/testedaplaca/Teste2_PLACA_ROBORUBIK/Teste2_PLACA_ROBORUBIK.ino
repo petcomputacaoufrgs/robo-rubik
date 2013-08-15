1501669e879f7a1d42ddbeb8a9732041a55109dc
Txai Wieser <txaidw@gmail.com>
2013-08-15 15:28:53 -0300

    updt3

--- Testes para Robo Rubik/testedaplaca/Teste2_PLACA_ROBORUBIK/Teste2_PLACA_ROBORUBIK.ino
+++ Testes para Robo Rubik/testedaplaca/Teste2_PLACA_ROBORUBIK/Teste2_PLACA_ROBORUBIK.ino
@@ -8,30 +8,30 @@
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
 
-const int DELAY=1500;
+#define DELAY  900
+#define ENTREONOFF  50
 
-
-int rele1 = 8;
-int rele2 = 2;
-int rele3 = 6;
-int rele4 = 7;
-int esq1 = 3;
-int esq2 = 10;
-int esq3 = 4;
-int esq4 = 5;
-int dir1 = 9;
-int dir2 = 11;
-int dir3 = 12;
-int dir4 = 13;
+#define DIR_ABRE  8
+#define DIR_FECHA  2
+#define ESQ_ABRE  6
+#define ESQ_FECHA  7
+#define esq1  3
+#define esq2  10
+#define esq3  4
+#define esq4  5
+#define dir1  9
+#define dir2  11
+#define dir3  12
+#define dir4  13
 
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(rele1, OUTPUT);
-  pinMode(rele2, OUTPUT);
-  pinMode(rele3, OUTPUT);
-  pinMode(rele4, OUTPUT);  
+  pinMode(DIR_ABRE, OUTPUT);
+  pinMode(DIR_FECHA, OUTPUT);
+  pinMode(ESQ_ABRE, OUTPUT);
+  pinMode(ESQ_FECHA, OUTPUT);  
   pinMode(esq1, OUTPUT);
   pinMode(esq2, OUTPUT);
   pinMode(esq3, OUTPUT);
@@ -40,14 +40,33 @@ void setup() {
   pinMode(dir2, OUTPUT);
   pinMode(dir3, OUTPUT);
   pinMode(dir4, OUTPUT);
+  
+  digitalWrite(DIR_FECHA, LOW);
+  digitalWrite(DIR_ABRE, LOW);
+  digitalWrite(ESQ_FECHA, LOW);
+  digitalWrite(ESQ_ABRE, LOW);
+  
+  delay(3000);
 }
   
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(rele2, LOW);
-  digitalWrite(rele1, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(DELAY);                // wait for a second
-  digitalWrite(rele2, LOW);
-  digitalWrite(rele1, HIGH);    // turn the LED off by making the voltage LOW
+  
   delay(DELAY);
+  
+  digitalWrite(DIR_ABRE, HIGH);
+  digitalWrite(ESQ_ABRE,HIGH);
+  delay(ENTREONOFF);
+  digitalWrite(DIR_ABRE, LOW);
+  digitalWrite(ESQ_ABRE,LOW);
+  
+  delay(DELAY);
+  
+  digitalWrite(DIR_FECHA, HIGH);
+  digitalWrite(ESQ_FECHA,HIGH);
+  delay(ENTREONOFF);
+  digitalWrite(DIR_FECHA, LOW);
+  digitalWrite(ESQ_FECHA,LOW);
+  
+  
 }
